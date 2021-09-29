5d54e0d3bb6036fb52aead04bdaafdbf797dcfd5
gotnatsarin <81910303+gotnatsarin@users.noreply.github.com>
2021-09-30 00:03:23 +0700

    เพิ่มหน้า login เพิ่มโฟลเดอร์จัดการต่าง ๆ

--- pages/callrecorderreport.php
+++ pages/callrecorderreport.php
@@ -1,15 +1,5 @@
-<?php 
-include('connect.php');
+<?php
 include('session/session.php');
-// $result = pg_query($conn, "SELECT * FROM recordings");
-
-// while($row = pg_fetch_assoc($result)){
-//     $myArray[]=$row;
-//   }
-  
-//  $jsonobj = json_encode($myArray,JSON_UNESCAPED_UNICODE);
-//  $arr2 = json_decode($jsonobj,true);
- 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,6 +22,8 @@ include('session/session.php');
 <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
 <!-- CSS Files -->
 <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+
+<!-- jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <style>
   audio{
@@ -44,13 +36,13 @@ include('session/session.php');
     <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
         <hr class="horizontal dark mt-0">
         <div class="collapse navbar-collapse  w-auto  max-height-vh-100 h-100" id="sidenav-collapse-main">
-        <?php include 'menu.php';?>
+        <?php include 'component/menu.php';?>
         </div>
     </aside>
     <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg ">
 
     <?php
-     include('navbar.php');
+     include_once('component/navbar.php');
     ?>
         <!-- <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
             <div class="container-fluid py-1 px-3">
@@ -90,7 +82,6 @@ include('session/session.php');
                 <div class="col-12">
                     <div class="card mb-4">
                         <div class="card-header pb-0">
-                            <h6>Detail</h6>
                         </div> 
                         <div class="card-body px-0 pt-0 pb-2">
                             <div class="table-responsive p-0">
@@ -136,7 +127,7 @@ include('session/session.php');
                 </div>
             </div>
             <?php 
-                include 'footer.php'
+                include 'component/footer.php'
             ?>
         </div> <br>
     </main>
